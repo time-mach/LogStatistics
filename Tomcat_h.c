35e89f2c9283f3e6a35e770bd3003b2518ce7f4b
time-mach <77321013+time-mach@users.noreply.github.com>
2021-01-19 17:51:54 +0800

    Add files via upload

---
+++ Tomcat_h.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <string.h>
+#include "Tomcat_h.h"
+#include "Common.h"
+#define HH_LEN 9
+
+/*
+ 本软件由江苏有线苏州分公司石鑫鹏开发，本软件所有源码不可用于任何商业目的，任何转发请标明作者和来源。若有疑问，可邮件联系：kelehaier@126.com
+*/
+
+
+void Tomcat_h(char *pLog)
+{
+	printf("Tomcat_h, 日志路径是 %s\n", pLog);
+	char HH[HH_LEN] = {'\0'};
+	char temp[HH_LEN] = {'\0'};
+	
+	int ret = 0;
+	SIZE count = 0;
+	
+	FILE * pFile = NULL;
+	if ( NULL == (pFile = fopen(pLog, "r")) ) {
+		printf("日志文件 %s 打开失败!\n", pLog);
+		return ;
+	}
+
+	ret = fscanf(pFile, "%*30c%s%*[^\n]", HH);
+	memset(HH + 2, 0, 7);
+	printf("=====读取参数的个数是 %d, 读取的时间是: %s\n", ret, HH);
+	count++;
+	ret = 0;
+	strcpy(temp, HH);
+	memset(HH, 0, HH_LEN);
+	
+	//可能当前就是最后一行，可能下一行与当前行一样，可能下一个行与当前行不一样 
+
+	while( !feof(pFile) )
+	{
+		ret = fscanf(pFile, "%*31c%s%*[^\n]", HH);
+		memset(HH + 2, 0, 7);
+		printf("=====读取参数的个数是 %d, 读取的时间是: %s\n", ret, HH);
+		if(0 == strcmp(temp, HH))
+		{
+			count++;
+			printf("日期 %s 的个数是 %u\n", temp, count);
+			ret = 0;
+			memset(HH, 0, HH_LEN);
+			continue;
+		}
+		//可能当前就是最后一行，可能下一行与当前行不一样，可能下一行与当前行一样 
+		//读取的是新的时间
+		//1 写入上一时间的个数
+		printf("上一个日期 %s 的个数是 %u\n", temp, count);
+		UpdateFile(temp, count); 
+		//2 count = 1
+		
+		//3 更新temp
+		memset(temp, 0, HH_LEN);
+		strcpy(temp, HH);
+		printf("更新日期是 %s\n", temp);
+		
+		//4 重置HH 
+		count = 1;
+		ret = 0;
+		memset(HH, 0, HH_LEN);
+		//可能当前就是最后一行，可能下一行与当前行不一样，可能下一行与当前行一样 
+	}
+	UpdateFile(temp, count); 
+	
+	fclose(pFile);
+	
+	return ;
+}
---
+++ main.c
@@ -0,0 +1,57 @@
+/*
+ 本软件由江苏有线苏州分公司石鑫鹏开发，本软件所有源码不可用于任何商业目的，任何转发请标明作者和来源。若有疑问，可邮件联系：kelehaier@126.com
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+#include "Tomcat.h"
+#include "Weblogic.h"
+#include "JBoss.h"
+#include "Common.h"
+
+#define SERVER_LEN 10
+#define SERVERS_COUNT 3
+#define LOGPATH_LEN 100
+#define PARAMS_COUNT 4
+
+int main(int argc,char *argv[]) //比方输入main.exe Tomcat -h /localhost_access_2020-01-01.txt,  argc就是4, 第一个参数是main.exe 第二个参数是 Tomcat 第三个参数是 -h 第四个参数是日志路径和日志名 
+{
+	char server[SERVER_LEN] = {'\0'};//存储命令行参数第二个参数 比如Tomcat 
+	//char form = '\0';//接收命令行第三个参数 比如-h -m -s
+	char log[LOGPATH_LEN] = {'\0'}; //接收命令行第四个参数 日志路径
+	
+	//char *SERVERS[SERVERS_COUNT] = {"Tomcat", "Weblogic", "JBoss"};
+	
+	if (PARAMS_COUNT != argc)//如果输入参数个数有误 
+	{
+		Help();
+		return 0;
+	}
+	
+	printf("第一个参数是: %s, 第二个参数是: %s, 第三个参数是: %s\n", argv[1], argv[2], argv[3]);
+	
+	if( 0 == strcmp(argv[1], "Tomcat") )
+	{
+		Tomcat(argv[2], argv[3]);
+	}
+	else if( 0 == strcmp(argv[1], "Weblogic") )
+	{
+		Weblogic(argv[2], argv[3]);
+	}
+	else if( 0 == strcmp(argv[1], "JBoss") )
+	{
+		JBoss(argv[2], argv[3]);
+	}
+	else
+	{
+		//扩充 
+	}
+	
+	puts("欢迎使用, 再见!");
+    system("pause");
+    
+	return 0;
+}
+
+
